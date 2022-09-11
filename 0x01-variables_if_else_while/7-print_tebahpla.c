114477f2e7a857873a2b057089970c24acb971cb
Great man <godswillidisi@gmail.com>
2022-09-10 22:01:00 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int alp;
+
+	for (alp = 'z'; alp >= 'a'; alp--)
+	putchar(alp);
+	putchar('\n');
+	return (0);
+	}
