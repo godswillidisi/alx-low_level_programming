e5e09ed445a529cb8b9224db0e341c6a189e0729
Great man <godswillidisi@gmail.com>
2022-09-10 21:18:42 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	printf("%i", digit);
+	putchar('\n');
+
+	return (0);
+
+}
