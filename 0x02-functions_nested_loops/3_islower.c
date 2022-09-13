2291b4ac845941ca2a392375484f59293fd99b7d
Great man <godswillidisi@gmail.com>
2022-09-13 10:53:50 -0700

    islower

---
+++ 0x02-functions_nested_loops/3_islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: return wither 1 or 0
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int _islower(int c)
+{
+	int c
+
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
