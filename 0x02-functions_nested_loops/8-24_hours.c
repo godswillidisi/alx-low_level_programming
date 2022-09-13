b778932c179bd4edab3fd1042fb023104870cc56
Great man <godswillidisi@gmail.com>
2022-09-13 13:37:23 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,40 +8,19 @@
 
 void jack_bauer(void)
 {
-	int hour_t, hour_i, minute_t, minute_i, hour;
+	int i;
+	int j;
 
-	hour = 58;
-	hour = '0';
-	while (hour_t < '3')
+	for (i = 0; i <= 23; i++)
 	{
-		if (hour_t == '2')
+		for (j = 0; j <= 59; j++)
 		{
-			hour = '4';
+			_putchar (i / 10 + '0');
+			_putchar (i % 10 + '0');
+			-putchar (':');
+			_putchar (j / 10 + '0');
+			_putchar (j % 10 + '0');
+			_putchar ('\n');
 		}
-		hour_i = '0';
-		while (hour_i < hour)
-		{
-			minute_t = '0';
-			while (minute_t < '6')
-			{
-				minute_i = '0';
-				while (minute_i < 58)
-				{
-					_putchar(hour_t);
-					_putchar(hour_i);
-					_putchar(':');
-					_putchar(minute_t);
-					_putchar(minute_i);
-					_putchar('\n');
-					minute_i++;
-				}
-				minute_i = '0';
-				minute_t++;
-			}
-			minute_t = '0';
-			hour_i++;
-		}
-		hour_i = '0';
-		hour_t++;
 	}
 }
