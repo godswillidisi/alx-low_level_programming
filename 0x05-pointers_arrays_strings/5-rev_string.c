b437b6beba1655312379142d5a3ac6e4f8c76944
Great man <godswillidisi@gmail.com>
2022-09-19 20:54:31 -0700

    A good engineer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,13 @@
 void rev_string(char *s)
 {
 	int a, size, half;
-	char first last;
+	char first, last;
 
-	a = 0
-		while (s[a] != '\0')
-		{
-			a++;
-		}
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
 	size = a - 1;
 	half = size / 2;
 	while (half >= 0)
