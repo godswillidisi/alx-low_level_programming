8bb62521425a382ba9946e92b5655c382c2112ce
Great man <godswillidisi@gmail.com>
2022-09-22 12:27:52 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,27 +2,27 @@
 
 /**
  * *_strncpy - Entry point
- * @dest:
- * @src
+ * @dest: pointer
+ * @src: pointer
  * @n: integer
  * Return: ALways 0
  */
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
-	int a;
+	int i;
 
-	a = 0;
-	while (a < n && src[a] != '\0')
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		dest[a] = src[a];
-		a++;
+		dest[i] = src[i];
+		i++;
 	}
 
-	while (a < n)
+	while (i < n)
 	{
-		dest[a] = '\0';
-		a++;
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
