f7e013683e2ef7ebefe362ada3683080872115bf
Great man <godswillidisi@gmail.com>
2022-09-22 12:46:34 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	char *str_1 = s1;
-	char *str_2 = s2;
-
-	while (*str_1 != '\0' && *str_2 != '\0' && *str_1 == *str_2)
+	while ((*s1 && s2) && (*s1 == *s2))
 	{
-		str_1++;
-		str_2++;
+		s1++;
+		s2++;
 	}
 
-	return (*str_1, *str_2);
+	return (*s1 - *s2);
 }
