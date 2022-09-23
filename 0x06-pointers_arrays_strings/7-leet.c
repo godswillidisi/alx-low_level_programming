8781442626dbb357d176b61dacb162967770c1a9
Great man <godswillidisi@gmail.com>
2022-09-22 18:42:38 -0700

    Everybody music

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - string to 1337
+ * @str: The string
+ *
+ * Return: A pointer
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == leet[j] ||
+					str[i] - 32 == leet[j])
+				str[i] = j + '0';
+		}
+		i++;
+	}
+
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int _putchar(char c);
+char *leet(char *str);
+
 #endif
