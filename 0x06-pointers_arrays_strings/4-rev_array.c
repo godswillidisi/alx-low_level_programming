6ccadc3f2bf4e633e78e084dd8a1ac1f275d955c
Great man <godswillidisi@gmail.com>
2022-09-22 18:02:11 -0700

    make me happy

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * reverse_array: Entry point
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int t, i;
+
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		t = a[n - 1 - i];
+		a[n - 1 -i] = a[i];
+		a[i] = t;
+	}
+}
