26da69be19ac8e20eac8427680d22a40d83f17cc
Great man <godswillidisi@gmail.com>
2022-09-22 18:52:25 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - Entry point
+ * @str: string
+ * Return: Always 0
+ */
+
+char *rot13(char *str)
+{
+	int c = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	CHAR ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + c) != '\0')
+	{
+
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + c) == alphabet[i])
+			{
+				*(s + c) = rot13[i];
+				break;
+			}
+			}
+		c++;
+	}
+
+	return (s);
+}
