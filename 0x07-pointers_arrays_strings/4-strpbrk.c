ffa77bff974cd85d285a9c0198851ddc85366670
Great man <godswillidisi@gmail.com>
2022-09-27 21:14:43 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searching a string for bytes
+ * @s: string
+ * @accept: bytes
+ * Return: Pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (NULL);
+}
