1d939be199e19338a9cf66e0f157c8e7fae7bb80
Great man <godswillidisi@gmail.com>
2022-10-06 21:29:44 -0700

    trust no one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,39 @@
 #include "main.h"
 /**
- * string_nconcat - 
- * @s1:
- * @s2: 
- * @n: 
- * Return: 
+ * string_nconcat - concatenates two strings.
+ * @s1: first sring
+ * @s2: second string
+ * @n: index
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	char *str;
+	unsigned int l1;
+	unsigned int l2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+	if (n >= l2)
+		n = l2;
+	str = malloc((l1 + l2 + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < l1; i++)
+	{
+		str[i] = s1[i];
+	}
+
+	for (; i < l1 + n; i++)
+	{
+		str[i] = s2[i - l1];
+	}
+	str[i] = '\0';
+	return (str);
+}
